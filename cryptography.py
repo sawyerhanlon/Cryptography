e76f499a1177ac4ec5296946cc9738afa7a854f0
sawyerhanlon <sawyerhanlon@hanovernorwichschools.org>
2015-11-19 14:36:05 -0500

    Updated from Brython Server: November 19, 2015 2:36:09 PM EST

--- cryptography.py
+++ cryptography.py
@@ -1,2 +1,71 @@
-testMr.Dennsison cryptography challenge
-run
+"""
+cryptography.py
+Author: Sawyer Hanlon
+Credit: Morgan Melliment
+
+Assignment:
+
+Write and submit a program that encrypts and decrypts user data.
+
+See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
+"""
+def evenstrings(message, key):
+    messlen = len(message)
+    keylen = len(key)
+    if keylen < messlen:
+        modkey1 = (messlen // keylen)*key
+        modkey2 = key[(messlen % keylen)-1]
+        return modkey1 + modkey2
+    elif keylen == messlen:
+        return key
+    else:
+        return key[messlen]
+a = True
+while a:
+    command = input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if command != "q":
+        associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+        message = input("Message: ")
+        key = input("Key: ")
+        elist = []
+        elist2 = []
+        elist3 = []
+        if command == "e":
+            modkey = evenstrings(message, key)
+            for a in message:
+                messa = associations.find(a)
+                elist.append(messa)
+            for b in modkey:
+                modkeya = associations.find(b)
+                elist2.append(modkeya)
+            ziplist = list(zip(elist,elist2))
+            for z in ziplist:
+                if z[0] + z[1] > 84:
+                    elist3.append((z[0]+z[1]) % 85)
+                else:
+                    elist3.append(z[0]+z[1])
+            for c in elist3:
+                print(associations[c], end='')
+            print('')
+        elif command == "d":
+            modkey = evenstrings(message, key)
+            for a in message:
+                messa = associations.find(a)
+                elist.append(messa)
+            for b in modkey:
+                modkeya = associations.find(b)
+                elist2.append(modkeya)
+            ziplist = list(zip(elist,elist2))
+            for z in ziplist:
+                if z[0] - z[1] < 0:
+                    elist3.append((z[0]-z[1]) + 85)
+                else:
+                    elist3.append(z[0]-z[1])
+            for c in elist3:
+                print(associations[c], end='')
+            print('')
+    elif command == "q":
+        print("Goodbye!")
+        a = False
+    else:
+        print("Did not understand command, try again.")
